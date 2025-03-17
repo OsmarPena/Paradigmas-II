deebecd528b964d1dc0956d8a281c34dc45ed63b
OsmarPena <alanpp1985@hotmail.com>
2025-03-16 19:10:53 -0600

    Add files via upload

---
+++ ParameterizedInterface/src/main/java/mx/uv/fiee/iinf/paradigmas/poo/IMinMax.java
@@ -0,0 +1,10 @@
+package mx.uv.fiee.iinf.paradigmas.poo;
+/*
+    Equipo:
+    zS22019636 Jorge Samuel Solano Dorantes
+    zs22002241 Alan Osmar Peña Polo
+*/
+public interface IMinMax<T> {
+    T min();
+    T max();
+}
